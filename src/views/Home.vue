64a67c346dce11dc65ed66ee314ab64085f83ae0
PaiZayOo <paizayoo.dev2022@gmail.com>
2023-12-22 22:21:14 +0630

    home

---
+++ src/views/Home.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class=" mt-3 flex justify-center ">
+        <input class=" rounded p-3 border-gray-300 w-72" placeholder="Enter your meal" type="text" name="" id="">
+    </div>
+    <div class="flex gap-1 justify-center mt-3">
+        <router-link  :to="{ name: 'byLetter', params: { letter } }" v-for="letter of letters" :key="letter">
+            {{ letter }}
+        </router-link>
+    </div>
+</template>
+
+<script setup>
+const letters = "ABCDEFGHIJKLMNOPQRSTUVW".split("");
+</script>
---
+++ src/views/MealList.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        Meal List
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
